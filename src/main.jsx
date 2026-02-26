c62ab6d6b3bf47e7838d4135cd4b879a623d6d8b
Ruan Lisboa <domruan333@gmail.com>
2026-02-26 14:45:25 -0300

    fix: launcher mute toggle on hover, bullet origin, CSS nesting, debug logs

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+<<<<<<< HEAD
+    <HashRouter>
+=======
         <BrowserRouter basename="/nana_/">
+>>>>>>> 72e7148 (fix: audio control launcher bugs, CSS 500 error, and expand language support)
             <App />
-        </BrowserRouter>
-    </React.StrictMode>,
+    </HashRouter>
+    </React.StrictMode >,
 )
